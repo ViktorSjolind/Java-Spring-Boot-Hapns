8559ffdb5efc00fb69ca57fffd09fda69b60085d
ViktorSjolind <viktor.sjolind@gmail.com>
2018-01-11 23:17:18 +0200

    Responsive menu

--- src/main/resources/static/js/validation.js
+++ src/main/resources/static/js/validation.js
@@ -5,6 +5,7 @@ var inputField = document.getElementById("create-post");
 var errorMessage = document.getElementsByClassName("error-message")[0];
 
 function validateDescLength(){
-	if(inputField.validity.tooLong)
-	
+	if(inputField.validity.tooLong){
+		alert("too long");
+	}
 }
